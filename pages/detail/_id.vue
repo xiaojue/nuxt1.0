ab8e48d5c59a937dd5a4b1b19359cc16c1606484
fuqiang6 <fuqiang6@staff.sina.com.cn>
2023-11-06 16:00:19 +0800

    first commit

---
+++ pages/detail/_id.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>{{ index }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(ctx){
+    let index = ctx.params.id;
+    return {
+      index
+    }
+  },
+  data: () => {
+    return {
+      page: "detail",
+    };
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ul class="list">
+      <li v-for="item in list">
+        <nuxt-link :to="{ name: 'detail-id', params: { id: item.index } }">{{
+          item.index
+        }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData(ctx) {
+    return new Promise((res) => {
+      setTimeout(() => {
+        let list = [];
+        for (var i = 0; i < 10; i++) {
+          list.push({
+            index: i,
+          });
+        }
+        res({
+          list,
+        });
+      }, 2000);
+    });
+  },
+  data: () => {
+    return {
+      page: "index",
+    };
+  },
+};
+</script>
+
+<style scoped>
+.list li {
+  height: 300px;
+  border-bottom: 1px solid #000;
+  text-align: center;
+}
+</style>
